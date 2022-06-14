721d14b18c71268dc294515a1c45be76d1c67444
AleLevi <ale.levi@hotmail.com>
2022-06-14 11:24:29 -0300

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.collection("mensagens").snapshots().listen((dado) {
+    dado.docs.forEach((d) {
+      print(d.data);
+    });
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
